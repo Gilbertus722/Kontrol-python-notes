ac35a1ca3c5e45b2a99cb869be1fdabad4c8ffb4
Vitalii <gilbertus722@yandex.ru>
2023-03-15 21:15:26 +0300

    aplic класс в котором пишем параметры  окну

--- note.py
+++ note.py
@@ -1,2 +1,23 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWindgets import QApplication, QmainWindow
+from PyQt5.QtWidgets import QApplication, QmainWindow, QMessageBox
+from PyQt5.QtCore import Qt, QTimer
+import sys
+import datetime
+
+def aplic():
+    app = QApplication(sys.argv)
+    okno = Window()
+
+    okno.show()
+    sys.exit(app.exec_())
+
+if __name__ == '_main_':
+    aplic
+
+class Window(QmainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle("progams")
+        self.setGeometry(400,400,600.300)
+    
